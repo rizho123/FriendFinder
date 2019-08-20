2924f29f238b134034e6a297809be652e38ead22
rizho123 <v-riczho@microsoft.com>
2019-08-20 01:14:47 -0700

    updating

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,3 +1,33 @@
 var path = require("path");
 var friends = require("../data/friends.js")
 
+module.exports = function (app) {
+
+    app.get("/api/friends", function(req, res) {
+        res.json(friends)
+    });
+
+    app.post("/api/friends"), function(req, res) {
+        var input = req.body;
+        var responses = input.scores;
+
+        var matchN = '';
+        var matchI = '';
+        var difference = 10000;
+
+        for (var i in friends) {
+            var diff = 0;
+            for (var j in response) {
+                diff += Math.abs(friends[i].scores[j] - responses[j]);
+            }
+
+            if(diff < difference) {
+                difference = diff;
+                matchN = friends[i].name;
+                matchI = friends[i].photo;
+            }
+        }
+        friends.push(input);
+        res.json({status: "OK", matchN: matchN, matchI: matchI})
+    }
+}
