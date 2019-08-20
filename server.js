076430f6928a2a9931c3ce91110bba8671fd67ad
rizho123 <v-riczho@microsoft.com>
2019-08-19 22:27:57 -0700

    adding package, requires

--- server.js
+++ server.js
@@ -5,5 +5,17 @@ var bodyParser = require("body-parser")
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+app.use(express.static(path.join(__dirname, './app/public')));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+
+require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
+
+app.listen(PORT, function() {
+  console.log('App is listening on PORT: ' + PORT);
+});
 
 
